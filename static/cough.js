414b98cbeed67b4006a0acea1fe1091f2f3a770a
General Aussie <paghulor@gmail.com>
2023-04-13 15:33:34 +0100

    fix result error

--- static/cough.js
+++ static/cough.js
@@ -1,25 +1,34 @@
 $(document).ready(function() {
-    $('#audio-submit').on('click', function(e) {
+  $('#audio-submit').on('click', function(e) {
       e.preventDefault();
   
       // Get form data
       var form_data = new FormData($('#upload-form')[0]);
+
+      // Add additional parameters to the form data
+      form_data.append('param1', $('#param1').val());
+      form_data.append('param2', $('#param2').is(':checked') ? 1 : 0);
+      form_data.append('gender', $('input[name=gender]:checked').val());
+      form_data.append('param4', $('#param4').is(':checked') ? 1 : 0);
   
       // Send AJAX request to upload audio and get result
       $.ajax({
-        type: 'POST',
-        url: '/upload',
-        data: form_data,
-        contentType: false,
-        processData: false,
-        success: function(result) {
-          // Update result on webpage
-          $('#result').text(result);
-        },
-        error: function() {
-          alert('Error uploading audio file!');
-        }
+          type: 'POST',
+          url: '/',
+          data: form_data,
+          contentType: false,
+          processData: false,
+          success: function(result) {
+              // Update result on webpage
+              $('#result').text(result);
+          },
+          error: function() {
+              alert('Error uploading audio file!');
+          }
       });
-    });
   });
-  
+});
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+}

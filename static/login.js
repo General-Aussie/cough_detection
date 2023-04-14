542e9e17ce17558f0774d650cd9bc038f489ea18
General Aussie <paghulor@gmail.com>
2023-04-14 12:45:58 +0100

    added disclaimer

--- static/login.js
+++ static/login.js
@@ -4,3 +4,10 @@ const continueBtn = document.getElementById('continue');
 checkbox.addEventListener('change', () => {
   continueBtn.disabled = !checkbox.checked;
 });
+
+const cont = document.getElementById('continue');
+const section = document.getElementById('section2');
+
+cont.addEventListener("click", () => {
+    section.classList.add("remov");
+});
